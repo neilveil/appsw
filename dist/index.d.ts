8015cdcc938db701e8f85f8e75deaa14ae73c32d
Neil Arya <neilarya.in@gmail.com>
2024-11-03 15:14:25 +0530

    Types added

---
+++ dist/index.d.ts
@@ -0,0 +1,9 @@
+type args = {
+    root: string;
+    type: 'static' | 'runtime';
+    uncompressed: boolean;
+    debug: boolean;
+    cli: boolean;
+};
+declare const _default: ({ root, type, uncompressed, debug, cli }: args) => void;
+export default _default;
